69ecc2b8a15d47df7e785212b0923334dea78830
Thanh Nha <thanhnha04042004@gmail.com>
2026-03-23 10:05:13 +0700

    add course

---
+++ src/main/java/com/echill/dto/response/CategoryResponse.java
@@ -0,0 +1,15 @@
+package com.echill.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CategoryResponse {
+    Long id;
+    String name;
+    String description;
+}
